abff5ea9c1473ac96321b2de6078f42c03a4f2a0
osmanaslancan <osmanaslancan@users.noreply.github.com>
2024-05-20 15:32:21 +0300

    add navigation to home page

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ModeToggle } from "../../components/mode-toggle";
 import { Separator } from "@/components/ui/separator";
+import { Link } from "react-router-dom";
 
 interface HeaderProps {
   children?: React.ReactNode;
@@ -10,10 +11,10 @@ function Header({ children }: HeaderProps) {
   return (
     <>
       <div className="flex flex-row w-full h-20 justify-between px-4 items-center">
-        <div className="flex items-center gap-2">
+        <Link to={"/"} replace className="flex items-center gap-2">
           <img src="/stajbuldum.webp" className="h-8 md:h-16 rounded-full" />
           <div className="text-l md:text-xl font-semibold italic">STAJBULDUM.NET</div>
-        </div>
+        </Link>
         <div className="flex space-x-2">
           {children}
           <ModeToggle />
